e8fd52f7cd2a5cb13f310bd1f10928c8cf4522bf
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-11-03 09:25:48 -0400

    parse

--- parser.py
+++ parser.py
@@ -5,18 +5,20 @@ from bs4 import BeautifulSoup
 class PoetryParser:
 
     def __init__(self, url):
-        self.poetry_foundation = 'https://www.poets.org/'
+        r = requests.get(url)
+        # print(r.text)
+        self.soup = BeautifulSoup(r.text, 'html.parser')
         self.url = url
 
     def parse(self):
-        r = requests.get(self.url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        return soup.find_all('pre')[0]
+        return self.soup.find_all('pre')[0]
+
+    def parse_found(self):
+        return self.soup.find_all('div', {"class": "o-poem"})
 
 
-
-
-if __name__ == '__main__':
-    p = PoetryParser('https://www.poets.org/poetsorg/poem/fly-0')
+if __name__=='__main__':
+    p = PoetryParser('https://www.poets.org/poetsorg/poem/fly-0');
+    # p1 = PoetryParser('https://www.poetryfoundation.org/poems/46304/retrospect-56d226248e844');
     print(p.parse())
 
